b5a21ab1ecec0c5ecea17c8a5882a5b720e635a4
Sandip Rajput <rsantosh63648@gmail.com>
2026-06-14 20:30:04 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import re
 
-def preprocess(data):
 
-    pattern = r'(\d{1,2}/\d{1,2}/\d{2},\s\d{1,2}:\d{2}\s?[ap]m)\s-\s([^:]+):\s(.*)'
+def preprocess(data, group_name="Unknown Group"):
+
+    pattern = r'(\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s?(?:am|pm)?)\s-\s([^:]+):\s(.*)'
 
     messages = re.findall(pattern, data, flags=re.IGNORECASE)
 
@@ -11,8 +12,12 @@ def preprocess(data):
 
     df['Date'] = pd.to_datetime(
         df['Date'],
-        format='%d/%m/%y, %I:%M %p'
+        errors='coerce',
+        dayfirst=True
     )
+
+    df = df.dropna(subset=['Date'])
+
     df['year'] = df['Date'].dt.year
     df['month_num'] = df['Date'].dt.month
     df['month'] = df['Date'].dt.month_name()
@@ -20,18 +25,17 @@ def preprocess(data):
     df['hour'] = df['Date'].dt.hour
     df['minute'] = df['Date'].dt.minute
     df['day_name'] = df['Date'].dt.day_name()
-    df['only_date']= df['Date'].dt.date
-    df['Message'] = df['Message'].replace(
-        '<Media omitted>',
-        'Media'
-    )
+    df['only_date'] = df['Date'].dt.date
+
+    df['Message'] = df['Message'].replace('<Media omitted>', 'Media')
+    df['Group'] = group_name
 
     period = []
-    for hour in df[['day_name', 'hour']]['hour']:
+    for hour in df['hour']:
         if hour == 23:
-            period.append(str(hour) + "-" + str('00'))
+            period.append("23-00")
         elif hour == 0:
-            period.append(str('00') + "-" + str(hour + 1))
+            period.append("00-1")
         else:
             period.append(str(hour) + "-" + str(hour + 1))
 
