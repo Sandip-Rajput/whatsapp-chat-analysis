4a816aca89323fe276d5523badd9911255f4b51b
Sandip Rajput <rsantosh63648@gmail.com>
2026-06-21 21:30:35 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import re
 
-
-def preprocess(data, group_name="Unknown Group"):
+def preprocess(data):
 
     pattern = r'(\d{1,2}/\d{1,2}/\d{2},\s\d{1,2}:\d{2}\s?[ap]m)\s-\s([^:]+):\s(.*)'
 
@@ -12,12 +11,8 @@ def preprocess(data, group_name="Unknown Group"):
 
     df['Date'] = pd.to_datetime(
         df['Date'],
-        format='%d/%m/%y, %I:%M %p',
-        errors='coerce'
+        format='%d/%m/%y, %I:%M %p'
     )
-
-    df = df.dropna(subset=['Date'])
-
     df['year'] = df['Date'].dt.year
     df['month_num'] = df['Date'].dt.month
     df['month'] = df['Date'].dt.month_name()
@@ -25,21 +20,18 @@ def preprocess(data, group_name="Unknown Group"):
     df['hour'] = df['Date'].dt.hour
     df['minute'] = df['Date'].dt.minute
     df['day_name'] = df['Date'].dt.day_name()
-    df['only_date'] = df['Date'].dt.date
-
+    df['only_date']= df['Date'].dt.date
     df['Message'] = df['Message'].replace(
         '<Media omitted>',
         'Media'
     )
 
-    df['Group'] = group_name
-
     period = []
-    for hour in df['hour']:
+    for hour in df[['day_name', 'hour']]['hour']:
         if hour == 23:
-            period.append("23-00")
+            period.append(str(hour) + "-" + str('00'))
         elif hour == 0:
-            period.append("00-1")
+            period.append(str('00') + "-" + str(hour + 1))
         else:
             period.append(str(hour) + "-" + str(hour + 1))
 
